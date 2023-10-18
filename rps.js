58e4d53ef86d7e0fd90b30016dbc82c6a359998a
dizmat <61530849+dizmat@users.noreply.github.com>
2023-10-18 14:51:13 +1100

    feat: implement rockPaperScissors game in console
    Player can interact with machine using a prompt box on webpage where
    they can type in their choice.

--- rps.js
+++ rps.js
@@ -1,7 +1,43 @@
 function getComputerChoice() {
-    let move = ["Rock", "Paper", "Scissors"];
-    const choice = Math.floor(Math.random() * 3);
-    console.log(move[choice]);
+	let moves = ['rock', 'paper', 'scissors'];
+	return moves[Math.floor(Math.random() * 3)];
 }
 
-getComputerChoice();
+function playRound(playerChoice, computerChoice) {
+	playerChoice.toLowerCase();
+
+	if (playerChoice == computerChoice) {
+		return 'tie';
+	} else if (
+		(playerChoice == 'rock' && computerChoice == 'paper') ||
+		(playerChoice == 'paper' && computerChoice == 'scissors') ||
+		(playerChoice == 'scissors' && computerChoice == 'rock')
+	) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function game() {
+	let playerScore = 0;
+	let computerScore = 0;
+	for (let i = 0; i <= 4; i++) {
+		let playerChoice = prompt('Enter move');
+		let computerChoice = getComputerChoice();
+
+		let result = playRound(playerChoice, computerChoice);
+		if (result == true) {
+			playerScore++;
+			console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
+		} else if (result == false) {
+			computerScore++;
+			console.log(`You Lose! ${computerChoice} beats ${playerChoice}`);
+		} else {
+			console.log(`Tie! ${computerChoice} matches ${playerChoice}`);
+		}
+		console.log(`Player: ${playerScore} , Computer: ${computerScore}`);
+	}
+}
+
+game();
